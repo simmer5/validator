8b8b69748348fc5e15b9930f040021b47bc920bb
Mindaugas <strumke@yahoo.com>
2020-10-23 10:06:13 +0200

    Updates

---
+++ precache-manifest.0508c422c4993ac1dbd0f24b61ae58de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef0011c718490d68eab78e67d999b5d",
+    "url": "/validator/index.html"
+  },
+  {
+    "revision": "3bd70c35a1a2ae4a21fd",
+    "url": "/validator/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "359877ff323531d02469",
+    "url": "/validator/static/js/2.0d993c57.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/validator/static/js/2.0d993c57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd70c35a1a2ae4a21fd",
+    "url": "/validator/static/js/main.6f1f9276.chunk.js"
+  },
+  {
+    "revision": "78f182d53cccd7ed5d0b",
+    "url": "/validator/static/js/runtime-main.7a22b425.js"
+  }
+]);
